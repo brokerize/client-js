aef3b6c53ceadaadb5ff8eecf5d2b88465b82fbf
Thorben Ziemek <thorben.ziemek@brokerize.com>
2026-02-18 17:49:40 +0100

    Revert "chore: update from spec 1.8.0"
    This reverts commit 08a9892f8c31a10835dc44526dbac97d4030ec20.

--- src/swagger/apis/index.ts
+++ src/swagger/apis/index.ts
@@ -1,15 +1,14 @@
 /* tslint:disable */
 /* eslint-disable */
 export * from "./AdminApi";
-export * from "./DecoupledOperationsApi";
+export * from "./BrokerLoginApi";
+export * from "./CancelOrderApi";
+export * from "./ChangeOrderApi";
+export * from "./DefaultApi";
 export * from "./DemobrokerApi";
 export * from "./ExportApi";
 export * from "./MetaApi";
-export * from "./OrderApi";
-export * from "./PortfolioApi";
 export * from "./SecuritiesApi";
-export * from "./SessionApi";
 export * from "./TradeApi";
 export * from "./TradeDraftApi";
 export * from "./UserApi";
-export * from "./WebsocketApi";
